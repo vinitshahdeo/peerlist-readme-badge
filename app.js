d7f213132bee8b19eedbca65de93ae27f24698c2
Vinit Shahdeo <vinit.shahdeo@postman.com>
2022-07-02 01:07:58 +0530

    added api to fetch svg image for badge

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var badgeRouter = require('./routes/badge');
 
 var app = express();
 
@@ -18,6 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/api', badgeRouter);
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
---
+++ routes/badge.js
@@ -0,0 +1,57 @@
+const express = require('express'),
+  router = express.Router(),
+  base64Img = require('base64-img'),
+  axios = require('axios'),
+  logo = base64Img.base64Sync('./public/images/peerlist.png'), // base64 of Peerlist logo
+  PEERLIST_DEFAULT_USERNAME = 'vinitshahdeo',
+  LABEL_COLOR = '00aa45',
+  BASE_BADGE_URL = 'https://img.shields.io/badge/Peerlist',
+  PEERLIST_PROFILE_BASE_URL = 'https://peerlist.io'
+
+  // returns the shields.io URL
+  getBadgeURL = (peerlistUsername) => {
+    const badgeURL = `${BASE_BADGE_URL}-${peerlistUsername}-brightgreen?link=${getProfileURL(peerlistUsername)}&logo=${logo}&color=${LABEL_COLOR}`;
+    return badgeURL;
+  },
+
+  // return peerlist profile URL
+  getProfileURL = (peerlistUsername) => {
+    const profileURL = `${PEERLIST_PROFILE_BASE_URL}/${peerlistUsername}`;
+    return profileURL;
+  },
+
+  // downloads badge in SVG
+  downloadBadge = async (badgeURL) => {
+    const response = await axios.get(badgeURL),
+      svgXml = response.data;
+    return svgXml;
+  };
+
+
+
+/* 
+GET /api/:peerlist-username 
+*/
+router.get('/:id', function(req, res, next) {
+  const peerlistUsername = req.params.id,
+    peerlistBadgeURL = getBadgeURL(peerlistUsername);
+
+    downloadBadge(peerlistBadgeURL).then((data) => {
+        res.send(data);
+    });
+});
+
+/* 
+GET /api (default username: vinitshahdeo) 
+*/
+router.get('/', function(req, res, next) {
+  const peerlistUsername = PEERLIST_DEFAULT_USERNAME,
+    peerlistBadgeURL = getBadgeURL(peerlistUsername);
+
+    downloadBadge(peerlistBadgeURL).then((data) => {
+      res.send(data);
+    });
+});
+
+
+module.exports = router;
