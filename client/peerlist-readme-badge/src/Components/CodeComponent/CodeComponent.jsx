e35697a794ab7209203b14bcd08fbbbff753bb50
Manthan Kuber <manthankuber873@gmail.com>
2022-09-29 17:47:48 +0530

    copy on click

--- client/peerlist-readme-badge/src/Components/CodeComponent/CodeComponent.jsx
+++ client/peerlist-readme-badge/src/Components/CodeComponent/CodeComponent.jsx
@@ -1,7 +1,24 @@
 import "../CodeComponent/CodeComponent.css";
-import { AiFillCopy } from "react-icons/ai";
+import { AiFillCopy, AiOutlineCheck } from "react-icons/ai";
+import { useState } from "react";
 
 const CodeComponent = ({ label, url }) => {
+  const [isCopied, setisCopied] = useState(false);
+
+  async function copyTextToClipboard(text) {
+    if ("clipboard" in navigator) {
+      return await navigator.clipboard.writeText(text);
+    } else {
+      return document.execCommand("copy", true, text);
+    }
+  }
+
+  const copyOnClick = () => {
+    copyTextToClipboard(url);
+    setisCopied(true);
+    setTimeout(() => setisCopied(false), 2500);
+  };
+
   return (
     <div className="CodeComponentContainer">
       <h3>{label}</h3>
@@ -12,7 +29,15 @@ const CodeComponent = ({ label, url }) => {
               <div>{url}</div>
             </code>
             <div className="CodeBlock__ButtonWrapper">
-              <AiFillCopy size={20} className="CodeBlock__Button" />
+              {isCopied ? (
+                <AiOutlineCheck size={20} className="CodeBlock__Button" />
+              ) : (
+                <AiFillCopy
+                  size={20}
+                  className="CodeBlock__Button"
+                  onClick={copyOnClick}
+                />
+              )}
             </div>
           </div>
         </pre>
