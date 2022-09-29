d9447a79c17ac09a1177bd3cec76636167d40a6a
Manthan Kuber <manthankuber873@gmail.com>
2022-09-29 11:12:52 +0530

    add app styles and jsx

--- client/peerlist-readme-badge/src/App.js
+++ client/peerlist-readme-badge/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Input from "./Components/Input/Input";
+import Select from "react-select";
+
+const options = [
+  { value: "flat", label: "flat" },
+  { value: "flat-square", label: "flat-square" },
+  { value: "plastic", label: "plastic" },
+  { value: "social", label: "social" },
+  { value: "for-the-badge", label: "for-the-badge" },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="MainContainer">
+      <h1 className="MainContainer__Title">Peerlist Readme Badge Generator</h1>
+      <h1 className="MainContainer__SubTitle">for README.md</h1>
+      <div className="FormContainer">
+        <form>
+          <Input label="Name" />
+          <Select options={options} className="FormContainer__Select" />
+          <button>Generate</button>
+        </form>
+      </div>
+    </main>
   );
 }
 
