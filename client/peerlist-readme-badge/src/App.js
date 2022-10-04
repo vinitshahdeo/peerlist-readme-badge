058ec7886a6dd60e24a2fd80a0fc99a9349db6f9
ramdhanihendrie <50083363+ramdhanihendrie@users.noreply.github.com>
2022-10-04 17:39:36 +0700

    Add an option to attach link

--- client/peerlist-readme-badge/src/App.js
+++ client/peerlist-readme-badge/src/App.js
@@ -3,6 +3,7 @@ import Input from "./Components/Input/Input";
 import CustomSelect from "./Components/CustomSelect/CustomSelect";
 import { useState } from "react";
 import Results from "./Components/Results/Results";
+import InputUsername from "./Components/InputUsername/InputUsername";
 
 const options = [
   { value: "flat", label: "flat" },
@@ -15,14 +16,19 @@ const options = [
 const initialFormState = {
   nameValue: "",
   selectValue: options[0].value,
+  peerlistUsername: ""
 };
 
 function App() {
   const [formState, setFormState] = useState(initialFormState);
   const [isSubmitted, setIsSubmitted] = useState(false);
+  const [isChecked, setIsChecked] = useState(false)
   const [badgeUrl, setBadgeUrl] = useState(
     `https://peerlist-readme-badge.herokuapp.com/api/${formState.nameValue}?style=${formState.selectValue}`
   );
+  const [linkProfile, setLinkProfile] = useState(
+    `https://peerlist.io/${formState.peerlistUsername}`
+  )
 
   const onSubmit = (e) => {
     e.preventDefault();
@@ -30,6 +36,12 @@ function App() {
       (prev) =>
         (prev = `https://peerlist-readme-badge.herokuapp.com/api/${formState.nameValue}?style=${formState.selectValue}`)
     );
+    if(isChecked){
+      setLinkProfile(
+        (prev) =>
+          (prev = `https://peerlist.io/${formState.peerlistUsername}`)
+      )
+    }
     setIsSubmitted(true);
   };
 
@@ -53,12 +65,35 @@ function App() {
             setFormState={setFormState}
             options={options}
           />
+          <div className="FormContainer__CheckboxWrapper">
+            <label>
+              <input 
+                type="checkbox"
+                checked={isChecked}
+                onChange={() => setIsChecked((prev) => !prev)}
+                className={isChecked ? "checked" : ""}
+              />
+              <span>Link to Peerlist profile</span>
+            </label>
+          </div>
+          {
+            isChecked && 
+              <InputUsername 
+                value={formState.peerlistUsername}
+                setFormState={setFormState}
+              />
+          }
           <div className="FormContainer__ButtonWrapper">
             <button className="FormContainer__Button">Generate</button>
           </div>
         </form>
       </div>
-      {isSubmitted && <Results badgeUrl={badgeUrl} />}
+      {isSubmitted && 
+        <Results 
+          badgeUrl={badgeUrl} 
+          linkProfile={linkProfile} 
+          isLinkToPeerlist={isChecked} 
+        />}
     </main>
   );
 }
--- client/peerlist-readme-badge/src/Components/Results/Results.jsx
+++ client/peerlist-readme-badge/src/Components/Results/Results.jsx
@@ -1,8 +1,9 @@
 import CodeComponent from "../CodeComponent/CodeComponent";
 import "../Results/Results.css";
 
-const Results = ({ badgeUrl }) => {
+const Results = ({ badgeUrl, linkProfile, isLinkToPeerlist }) => {
   const badgeMdUrl = `![Peerlist](${badgeUrl})`;
+  const badgeMdUrlLink = `[![Peerlist](${badgeUrl})](${linkProfile})`;
   const htmlUrl = `<img src="${badgeUrl}" alt="Peerlist Badge" />`;
 
   return (
@@ -10,7 +11,7 @@ const Results = ({ badgeUrl }) => {
       <div className="ResultsContainer__ImageWrapper">
         <img src={badgeUrl} alt="badge" />
       </div>
-      <CodeComponent label={"Markdown"} url={badgeMdUrl} />
+      <CodeComponent label={"Markdown"} url={isLinkToPeerlist ? badgeMdUrlLink : badgeMdUrl} />
       <CodeComponent label={"Html"} url={htmlUrl} />
     </div>
   );
